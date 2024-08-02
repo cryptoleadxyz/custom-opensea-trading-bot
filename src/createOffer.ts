b10288b2a74044512550946f17f82d7b245ab9df
cryptoleadxyz <cryptolead.eth@gmail.com>
2024-08-02 16:25:17 -0600

    rename variable naming and fill target nft detail

--- src/createOffer.ts
+++ src/createOffer.ts
@@ -1,35 +1,37 @@
-import { WALLET_ADDRESS, sdk} from './utils/constants';
+import { WALLET_ADDRESS, sdk } from "./utils/constants";
 
 const createOffer = async () => {
-    
-    // TODO: Fill in the token address and token ID of the NFT you want to make an offer on, as well as the price
-    let tokenAddress: string = "";
-    let tokenId: string = "";
-    let offerAmount: string = "";
+  // TODO: Fill in the token address and token ID of the NFT you want to make an offer on, as well as the price
+  let tokenAddress: string = "0xe29f8038d1a3445ab22ad1373c65ec0a6e1161a4";
+  let tokenId: string = "316";
+  let offerAmount: string = "0.000001";
 
-    const offer = {
-        accountAddress: WALLET_ADDRESS,
-        startAmount: offerAmount,
-        asset: {
-            tokenAddress: tokenAddress,
-            tokenId: tokenId,
-        },
-    };
+  const offer = {
+    accountAddress: WALLET_ADDRESS,
+    startAmount: offerAmount,
+    asset: {
+      tokenAddress: tokenAddress,
+      tokenId: tokenId,
+    },
+  };
 
-    try {
-        const response = await sdk.createOffer(offer);
-        console.log("Successfully created an offer with orderHash:", response.orderHash);
-    } catch (error) {
-        console.error("Error in createOffer:", error);
-    }
-}
+  try {
+    const response = await sdk.createOffer(offer);
+    console.log(
+      "Successfully created an offer with orderHash:",
+      response.orderHash
+    );
+  } catch (error) {
+    console.error("Error in createOffer:", error);
+  }
+};
 
 // Check if the module is the main entry point
 if (require.main === module) {
-    // If yes, run the createOffer function
-    createOffer().catch((error) => {
-        console.error("Error in createOffer:", error);
-    });
+  // If yes, run the createOffer function
+  createOffer().catch((error) => {
+    console.error("Error in createOffer:", error);
+  });
 }
 
 export default createOffer;
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,20 +5,17 @@ export const OPENSEA_API_KEY = process.env.OPENSEA_API_KEY;
 export const WALLET_PRIV_KEY = process.env.WALLET_PRIV_KEY;
 export const ALCHEMY_API_KEY_MAINNET = process.env.ALCHEMY_API_KEY;
 
-let provider = new AlchemyProvider("homestead", ALCHEMY_API_KEY_MAINNET)
+let provider = new AlchemyProvider("homestead", ALCHEMY_API_KEY_MAINNET);
 
-export const walletMainnet = new ethers.Wallet(
-    WALLET_PRIV_KEY as string,
-    provider
-);
+export const walletObj = new ethers.Wallet(WALLET_PRIV_KEY as string, provider);
 
-export const WALLET_ADDRESS = walletMainnet.address;
+export const WALLET_ADDRESS = walletObj.address;
 
 export const sdk = new OpenSeaSDK(
-    walletMainnet,
-    {
-        chain: Chain.Mainnet,
-        apiKey: OPENSEA_API_KEY,
-    },
-    (line) => console.info(`MAINNET: ${line}`),
+  walletObj,
+  {
+    chain: Chain.Mainnet,
+    apiKey: OPENSEA_API_KEY,
+  },
+  (line) => console.info(`MAINNET: ${line}`)
 );
