dab0e7e6b3d698402d50bf1a1d681ea01ea21b54
cryptoleadxyz <cryptolead.eth@gmail.com>
2024-08-07 13:05:45 -0600

    enable control `expirationTime`

--- src/createOffer.ts
+++ src/createOffer.ts
@@ -3,12 +3,14 @@ import { WALLET_ADDRESS, sdk } from "./utils/constants";
 const createOffer = async () => {
   // TODO: Fill in the token address and token ID of the NFT you want to make an offer on, as well as the price
   let tokenAddress: string = "0xe29f8038d1a3445ab22ad1373c65ec0a6e1161a4";
-  let tokenId: string = "316";
-  let offerAmount: string = "0.0888";
+  let tokenId: string = "166";
+  let offerAmount: string = "0.0001"; // in eth
+  let expirationTime: number = Math.round(Date.now() / 1000 + 10 * 60); // denominator can be configured as in second, min, hour, etc...
 
   const offer = {
     accountAddress: WALLET_ADDRESS,
     startAmount: offerAmount,
+    expirationTime: expirationTime,
     asset: {
       tokenAddress: tokenAddress,
       tokenId: tokenId,
